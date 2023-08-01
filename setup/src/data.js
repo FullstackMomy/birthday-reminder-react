b60fe542fad6aa51e34fbbb3c32ec614b6f88003
ayala <ayalashulman1@gmail.com>
2023-08-01 21:14:51 +0300

    1

---
+++ setup/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: "Ayala Shulman",
+    age: 27,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+  },
+  {
+    id: 2,
+    name: "Ben Cohen",
+    age: 45,
+    image:
+      "https://cdn.pixabay.com/photo/2016/11/29/01/34/man-1866574__340.jpg",
+  },
+  {
+    id: 3,
+    name: "Lior Tohalian",
+    age: 27,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+  {
+    id: 4,
+    name: "Gil Shterman",
+    age: 42,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    name: "Sendi The Second",
+    age: 12,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+];
